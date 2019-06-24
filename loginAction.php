1b7ceb080356d192e9e98d2050b18553fb253b17
YuriDUAN <2396139847@qq.com>
2019-06-24 22:18:30 +0800

    加入申请界面

--- loginAction.php
+++ loginAction.php
@@ -8,14 +8,12 @@ if (!empty($_POST)){
     $result=$conn->query($sql);
 
     while ($row=mysqli_fetch_array($result)){
-        if (empty($row)){
-            echo '<script>alert("用户名或密码不存在");window.location.href="index.html"</script>';
-            return;
-        }
-        if ($row['sno'] == $sno && $row['password']== $psd){
+       if ($row['sno'] == $sno && $row['password']== $psd){
             $_SESSION['username'] = $row['username'];
             $_SESSION['sno'] = $sno;
             echo '<script>alert("登陆成功，即将跳转！");window.location.href="homepage.html"</script>';
+        }else{
+            echo '<script>alert("用户名或密码错误");window.location.href="index.html"</script>';
         }
     }
     mysqli_free_result($result);
