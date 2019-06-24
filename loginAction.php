c2e8c0c23e436c81a3f070ef01d2c505d48b3cbf
YuriDUAN <2396139847@qq.com>
2019-06-24 08:28:57 +0800

    Merge branch 'master' of D:\IDEAIU\Project\EasyTeam with conflicts.

--- loginAction.php
+++ loginAction.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
 include "MySqlConnect.php";
+
 if (!empty($_POST)){
-    $sno = $_POST["sno"];
+    $name = $_POST["username"];
     $psd = $_POST["password"];
-    $sql="select * from ruser";
+    $sql="select * from chart ";
     $result=$conn->query($sql);
 
     while ($row=mysqli_fetch_array($result)){
@@ -12,10 +13,10 @@ if (!empty($_POST)){
             echo '<script>alert("用户名或密码不存在");window.location.href="login.html"</script>';
             return;
         }
-        if ($row['sno'] == $sno && $row['password']== $psd){
+        if ($row['username'] == $name && $row['password']== $psd){
             $_SESSION['username'] = $row['username'];
-            $_SESSION['sno'] = $sno;
-            echo '<script>alert("登陆成功，即将跳转！");window.location.href="index.html"</script>';
+            $_SESSION['uid'] = $row["id"];
+           echo '<script>alert("登陆成功，即将跳转！");window.location.href="index.html"</script>';
         }
     }
     mysqli_free_result($result);
