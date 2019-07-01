0b0824f4cc94dc5b12d1fd5baf94872b31bfaf63
YuriDUAN <2396139847@qq.com>
2019-07-01 09:09:43 +0800

    修改部分bug

--- activityCollectAction.php
+++ activityCollectAction.php
@@ -6,7 +6,7 @@ if (!empty($_POST['arid'])&&!empty($_POST['url'])){
     $url = $_POST['url'];
     $c_sno = $_SESSION['sno'];
     $c_time = date("Y-m-d");
-    $sqlurl = "select c_ar_id from collect where c_ar_id = $c_ar_id";
+    $sqlurl = "select c_ar_id from collect where c_ar_id = $c_ar_id and c_sno = $c_sno";
     $resulturl = $conn->query($sqlurl);
     if (empty($rowurl = mysqli_fetch_array($resulturl))){
         $sql = "insert into collect(c_ar_id,url,c_sno,c_time)values ('$c_ar_id','$url','$c_sno','$c_time')";
